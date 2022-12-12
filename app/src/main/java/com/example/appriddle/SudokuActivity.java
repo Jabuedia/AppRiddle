611485e128e5f9d8a5e92f75f2e2a1278c71d94a
Jbuendia <jabuendia19@gmail.com>
2022-12-12 08:38:40 -0500

    sudoku activity.JB

--- app/src/main/java/com/example/appriddle/SudokuActivity.java
+++ app/src/main/java/com/example/appriddle/SudokuActivity.java
@@ -2,14 +2,26 @@ package com.example.appriddle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SudokuActivity extends AppCompatActivity {
-
+    TextView nose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
         getSupportActionBar().hide(); // Esconder la action bar asquerosa
+
+        nose = findViewById(R.id.regresar);
     }
+
+    private void atras(){
+        Intent intent = new Intent(SudokuActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
